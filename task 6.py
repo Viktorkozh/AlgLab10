9e40a5ad8357d53f92f57470c468d914e9ca9379
ViktorKozh <viktor.ru253@gmail.com>
2023-12-17 14:26:19 +0300

    update to task6

--- task 6.py
+++ task 6.py
@@ -4,24 +4,29 @@ import random
 
 def print_ascending_sums(A, B):
 
+    # Сортируем оба массива
     A.sort()
     B.sort()
 
-    n = len(A)
-    
-    # Внешний цикл для каждого элемента из массива A
-    for i in range(n):
-        # Внутренний цикл для каждого элемента из массива B
-        for j in range(n):
-            print(A[i] + B[j], end=' ')
+    # min-heap хранит выражения вида (сумма, индекс в A, индекс в B)
+    heap = [(A[i] + B[0], i, 0) for i in range(len(A))]
+    heapq.heapify(heap)
+
+    # Вывод суммы в возрастающем порядке
+    for _ in range(len(A)**2):
+        sum, i, j = heapq.heappop(heap)
+        print(sum, end=' ')
+        if j + 1 < len(B):
+            heapq.heappush(heap, (A[i] + B[j+1], i, j+1))
 
 
 def fill_list(num_of_elements):
-    a = [random.randint(0, 10) for _ in range(num_of_elements)]
+    a = [random.randint(1, 10) for _ in range(num_of_elements)]
     return a
 
 
-A = fill_list(3)
-B = fill_list(3)
+n = 5
+A = fill_list(n)
+B = fill_list(n)
 print(sorted(A), "\n", sorted(B))
 print_ascending_sums(A, B)
